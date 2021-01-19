f1d9ccde8d25c7c77c6908f930d4df4db20781ec
Aryan Gaurav <aryangaurav0504@gmail.com>
2021-01-19 16:05:06 +0530

    Add files via upload

---
+++ 2.2.Newton_Raphson.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+double fx(double x)
+{
+    return x-cos(x);
+}
+double f1x(double x)
+{
+    return 1+sin(x);
+}
+int main()
+{
+    double x0,x1,d=1e-5;
+    cout<<"Enter the intial guess\n";
+    cin>>x0;
+    int n;
+    cout<<"Enter the number of iterations\n";
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        if(fabs(f1x(x0))<d) //slope is too low for updation
+            break;
+        x1=x0-fx(x0)/f1x(x0);
+        if(fabs((x1-x0)/x1)<1e-5)   //relative difference is low i.e. converged
+            break;
+        else x0=x1;
+    }
+    printf("%.6lf",x0);
+    return 0;
+}
